e95b959087ccb0651062c95b1e5cd90dde3752c8
Momchil Atanasov <momchil.dev@gmail.com>
2023-01-19 22:42:11 +0200

    Add FromPtr and ToPtr helper functions to the opt package (#10)

--- opt/value_test.go
+++ opt/value_test.go
@@ -20,4 +20,29 @@ var _ = Describe("Optional", func() {
 		Expect(v.Value).To(Equal("hello"))
 	})
 
+	It("is possible to create an unspecified value from pointer", func() {
+		v := opt.FromPtr[int](nil)
+		Expect(v.Specified).To(BeFalse())
+	})
+
+	It("is possible to create a specified value from pointer", func() {
+		actual := 10
+		v := opt.FromPtr(&actual)
+		Expect(v.Specified).To(BeTrue())
+		Expect(v.Value).To(Equal(actual))
+	})
+
+	It("is possible to get a pointer representation of an unspecified value", func() {
+		v := opt.Unspecified[string]()
+		ptr := v.ToPtr()
+		Expect(ptr).To(BeNil())
+	})
+
+	It("is possible to get a pointer representation of a specified value", func() {
+		v := opt.V("hello")
+		ptr := v.ToPtr()
+		Expect(ptr).ToNot(BeNil())
+		Expect(*ptr).To(Equal("hello"))
+	})
+
 })
