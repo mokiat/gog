614ca2e6835aa7cce08b883f3af7ca0ef2acd4b5
Momchil Atanasov <momchil.dev@gmail.com>
2025-04-19 00:46:25 +0300

    Add a Must function

--- value.go
+++ value.go
@@ -5,3 +5,13 @@ func Zero[T any]() T {
 	var zero T
 	return zero
 }
+
+// Must is a helper function that can be used to quickly extract a value from a
+// function that returns a value and an error. It panics if the error is not
+// nil.
+func Must[T any](v T, err error) T {
+	if err != nil {
+		panic(err)
+	}
+	return v
+}
